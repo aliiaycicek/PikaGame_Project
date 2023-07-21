2c76b3c303660fc24771d17e2e4323eaf2c84b58
Ali Aycicek <aliaycicek_7010@hotmail.com>
2023-07-21 16:14:37 +0300

    Pika Project added.

---
+++ RankingVC.swift
@@ -0,0 +1,41 @@
+//
+//  RankingVC.swift
+//  YakalamaOyunu
+//
+//  Created by Ali Ayçiçek on 19.07.2023.
+//
+
+import UIKit
+
+class RankingVC: UIViewController {
+    
+    
+    var playerNames = [String]()
+    
+    @IBOutlet weak var rankingLable: UILabel!
+    
+    @IBOutlet weak var rankingTableView: UITableView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 10
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = UITableViewCell()
+            var content = cell.defaultContentConfiguration()
+            content.text = playerNames[indexPath.row]
+            cell.contentConfiguration = content
+            return cell
+        }
+    
+}
