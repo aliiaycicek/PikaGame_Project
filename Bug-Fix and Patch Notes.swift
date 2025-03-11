40cac2313ee9b0c7e29d5c7c0301c0019804c1ec
Ali AYÇİÇEK <aliaycicek_7010@hotmail.com>
2025-03-11 11:30:20 +0300

    Task update

--- Bug-Fix and Patch Notes.swift
+++ Bug-Fix and Patch Notes.swift
@@ -22,18 +22,18 @@
 
 // Seçmeli Time eklenicek kullancı oyuna başlarken 3 farklı zaman seçeneği sunulucak bunlarla birlikte her farklı zaman seçeneği için 2 farklı zorluk seviyesi olucak pikachuların ve darkSide pikachuların çıkış hızı ve sayısı değişicek! [TAMAMLANDI] ++
 
-// Ses dosyaları eklenicek! [TAMAMLANDI] +
-
 // AppIcon ve yeni skinler eklenicek. [TAMAMLANDI] ++
 
 // SkinVC adında bir view controller eklenip buraya kullanıcının pikachularını seçebilmesi için farklı pikachu skinleri eklenicek! [TAMAMLANDI] ++
 
-// SkinVC deki bazı skinler para ile açılabilicekken bazı skinler için yeni bir achivment adında bir vcdeki bazı taskları kullanıcnın tamamlamasına göre belirli pika skinleri kazanbilicek!(bu sayede ödül sistemi olucak ve oyunun oynanırlığı artıcak)     <---- Güncel Patch
-
-// Zaman bittiğinde kullanıcnın ek 5 saniye zaman alabilmesi için 1 sefere mahsus para/reklam izle komutları eklenerek gelir kısmın ayarlanıcak!
-
 // SettingsVC oluşturulacak ve ses ayarları ve oyun optimizasyonuna dair bazı paneller konulacak! [TAMAMLANDI] ++
 
 // Ranking Sistemi eklenicek ve kullanıcı kendi yaptığı highscore ları görebilicek! [TAMAMLANDI] ++
+
+// Ses dosyaları eklenicek! [TAMAMLANDI] +
+
+// SkinVC deki bazı skinler para ile açılabilicekken bazı skinler için yeni bir achivment adında bir vcdeki bazı taskları kullanıcnın tamamlamasına göre belirli pika skinleri kazanbilicek!(bu sayeide ödül sistemi olucak ve oyunun oynanırlığı artıcak)     <---- Güncel Patch
+
+// Zaman bittiğinde kullanıcnın ek 5 saniye zaman alabilmesi için 1 sefere mahsus para/reklam izle komutları eklenerek gelir kısmın ayarlanıcak!
 
 // Ödeme sistemi düzenlenicek.(opsiyonel)
